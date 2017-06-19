70efcdb0c5550868e225c8919d369e287302355d
terashima <terashima@cs-service.co.jp>
2017-06-19 09:59:47 +0900

    bugfix

--- src/MarketplaceWebServiceProducts/Client.php
+++ src/MarketplaceWebServiceProducts/Client.php
@@ -896,9 +896,9 @@ class MarketplaceWebServiceProducts_Client implements MarketplaceWebServiceProdu
      */
     public function __construct($awsAccessKeyId, $awsSecretAccessKey, $applicationName, $applicationVersion, $config = null)
     {
-        iconv_set_encoding('output_encoding', 'UTF-8');
-        iconv_set_encoding('input_encoding', 'UTF-8');
-        iconv_set_encoding('internal_encoding', 'UTF-8');
+        @iconv_set_encoding('output_encoding', 'UTF-8');
+        @iconv_set_encoding('input_encoding', 'UTF-8');
+        @iconv_set_encoding('internal_encoding', 'UTF-8');
 
         $this->_awsAccessKeyId = $awsAccessKeyId;
         $this->_awsSecretAccessKey = $awsSecretAccessKey;
